a1634ca95088509ffc21b284659ab65a38635026
Micheal Gao <mich.gao1122@gmail.com>
2023-10-23 14:42:23 -0400

    Create sign up output data

--- src/use_case/signup/SignUpOutputData.java
+++ src/use_case/signup/SignUpOutputData.java
@@ -1,4 +1,23 @@
 package use_case.signup;
 
-public class SignUpOutputData {
+public class SignUpOutputData{
+
+    private final String username;
+    private boolean useCaseFailed;
+
+    public SignUpOutputData(String username, boolean useCaseFailed)
+    {
+        this.username = username;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public boolean getUseCaseFailed()
+    {
+        return useCaseFailed;
+    }
 }
