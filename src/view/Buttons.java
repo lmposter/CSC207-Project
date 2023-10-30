abda1474daaf590492acab1a9c6d677e984e3dff
Pumpkinmann <jackyshen1011@gmail.com>
2023-10-30 13:46:06 -0400

    initalize buttons and content for java swing front_end design

---
+++ src/view/Buttons.java
@@ -0,0 +1,11 @@
+package view;
+
+
+import javax.swing.*;
+
+public class Buttons extends JPanel {
+    Buttons(JLabel button, JTextField content) {
+        this.add(button);
+        this.add(content);
+    }
+}
