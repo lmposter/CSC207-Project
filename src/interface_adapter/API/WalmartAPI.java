580c8cb7f1013c3608f757e7b75a5a4bc5e5ed2f
youyang <youyang.guo@mail.utoronto.ca>
2023-11-19 19:43:45 -0500

    fix and test api works, made token environmental

--- src/interface_adapter/API/WalmartAPI.java
+++ src/interface_adapter/API/WalmartAPI.java
@@ -9,13 +9,15 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
+
 public class WalmartAPI {
 
-    public ArrayList<Product> searchWalmart(String[] searchItems)  {
+    public static ArrayList<Product> searchWalmart(String[] searchItems)  {
         try {
 
             HttpURLConnection connection = getHttpURLConnection(searchItems);
@@ -36,14 +38,14 @@ public class WalmartAPI {
                     String title = product.getString("title");
                     String id = product.getString("product_id");
                     String photo = product.getString("main_image");
-                    int stars = Integer.parseInt(product.getString("rating"));
+                    int stars = product.getInt("rating");
                     JSONObject inventory = result.getJSONObject("inventory");
-                    boolean inStock = Boolean.parseBoolean(inventory.getString("in_stock"));
+                    boolean inStock = inventory.getBoolean("in_stock");
                     int numInventory = 0;
                     if (inStock){numInventory = 1;}
                     JSONObject offers = result.getJSONObject("offers");
                     JSONObject primary = offers.getJSONObject("primary");
-                    double price = Double.parseDouble(primary.getString("price"));
+                    double price = primary.getDouble("price");
                     Product pd = productFactory.create(title, photo, price, numInventory, new ArrayList<Tag>());
                     Review review = new Review(stars, "");
                     pd.addReview(review);
@@ -86,11 +88,15 @@ public class WalmartAPI {
     private static HttpURLConnection getHttpURLConnection(String[] searchItems) throws IOException {
         String contentS = searchItems[0];
         for (int i = 1; i < searchItems.length; i++){
-            contentS.concat("+");
-            contentS.concat(searchItems[i]);
+            contentS = contentS.concat("+").concat(searchItems[i]);
         }
-        // Create a URL object with the endpoint you want to request
-        URL url = new URL("https://api.bluecartapi.com/request?api_key=D3C7BEE795534D07B1BCCB0A977C0325&type=search&search_term=".concat(contentS));
+
+//        System.out.println(contentS);
+        String apiToken = System.getenv("API_TOKEN_WAL");
+        String photoURL = "https://api.bluecartapi.com/request?api_key=".concat(apiToken).concat("&search_term=").concat(contentS).concat("&type=search");
+        //"https://api.bluecartapi.com/request?api_key={apiToken}&type=search&search_term=".concat(contentS)
+        System.out.println(photoURL);
+        URL url = new URL(photoURL);
         //open a connection to the URL
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
@@ -99,6 +105,15 @@ public class WalmartAPI {
         return connection;
     }
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+//        code for testing, uncomment to test
+//        String apiToken = System.getenv("API_TOKEN_WAL");
+//        System.out.println(apiToken);
+//
+//        String[] x = new String[2];
+//        x[0] = "pen";
+//        x[1] = "apple";
+//        System.out.println(searchWalmart(x));
+    }
     public WalmartAPI(){}
 }
