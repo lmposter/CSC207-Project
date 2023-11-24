5e1c4ae722a3552ee39875f58dbbc33065a7ba0a
Michael Gao <mich.gao@mail.utoronto.ca>
2023-11-23 21:13:51 -0500

    Imeplement ShoppingCartView

--- src/view/ShoppingCartView.java
+++ src/view/ShoppingCartView.java
@@ -1,8 +1,11 @@
 package view;
 
+import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.shopping_cart.ShoppingCartViewModel;
+import interface_adapter.shopping_cart.checkOut.CheckOutController;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -14,12 +17,66 @@ public class ShoppingCartView extends JPanel implements ActionListener, Property
 
     private final ShoppingCartViewModel shoppingCartViewModel;
 
-    private final JButton checkOut;
+    private final CheckOutController checkOutController;
 
-    public ShoppingCartView(ShoppingCartViewModel shoppingCartViewModel, JButton checkOut)
+    private final LoggedInViewModel loggedInViewModel;
+
+    private final JButton checkOut;
+    private final JButton store;
+
+    public ShoppingCartView(ShoppingCartViewModel shoppingCartViewModel, CheckOutController checkOutController, LoggedInViewModel loggedInViewModel)
     {
         this.shoppingCartViewModel = shoppingCartViewModel;
-        this.checkOut = checkOut;
+        this.checkOutController = checkOutController;
+        this.loggedInViewModel = loggedInViewModel;
+
+        this.checkOut = new JButton("Check Out");
+        this.store = new JButton("Store");
+
+        this.shoppingCartViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel("Shopping Cart");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel buttons = new JPanel();
+        buttons.add(this.checkOut);
+        buttons.add(this.store);
+
+        checkOut.addActionListener(
+                new ActionListener()
+                {
+                    @Override
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        if (e.getSource().equals(checkOut))
+                        {
+                            String username = loggedInViewModel.getLoggedInUser();
+                            checkOutController.execute(username);
+                        }
+                    }
+                }
+        );
+
+        store.addActionListener(
+                new ActionListener()
+                {
+                    @Override
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        if (e.getSource().equals(store))
+                        {
+                            //TODO Switch to Store Page
+                        }
+                    }
+                }
+        );
+
+
+    }
+
+    private JPanel createProductPanel(String name, double price)
+    {
+
     }
 
     @Override
