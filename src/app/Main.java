546274805e5c666e78339bb722917f5916a6f0ee
Pumpkinmann <jackyshen1011@gmail.com>
2023-11-27 14:19:21 -0500

    edit search view

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,24 +1,24 @@
 package app;
 
+import data_access.ProductDAO;
 import data_access.UserDataAccessObject;
 import entity.BuyerFactory;
+import entity.ProductFactory;
 import entity.SellerFactory;
-import interface_adapter.login.LoginViewModel;
-import interface_adapter.logged_in.LoggedInViewModel;
-import interface_adapter.signup.SignupViewModel;
 import interface_adapter.ViewManagerModel;
-import use_case.login.LoginUserDataAccessInterface;
-import view.LoggedInView;
-import view.LoginView;
-import view.SignupView;
-import view.ViewManager;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.search.SearchViewModel;
+import interface_adapter.signup.SignupViewModel;
+import view.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Build the main program window, the main panel containing the
         // various cards, and the layout, and stitch them together.
 
@@ -36,13 +36,19 @@ public class Main {
         ViewManagerModel viewManagerModel = new ViewManagerModel();
         new ViewManager(views, cardLayout, viewManagerModel);
 
-        // The data for the views, such as username and password, are in the ViewModels.
+        // The data for the views, such as use  rname and password, are in the ViewModels.
         // This information will be changed by a presenter object that is reporting the
         // results from the use case. The ViewModels are observable, and will
         // be observed by the Views.
         LoginViewModel loginViewModel = new LoginViewModel();
         LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
         SignupViewModel signupViewModel = new SignupViewModel();
+        SearchViewModel searchViewModel = new SearchViewModel();
+        FileWriter fileWriter = new FileWriter("empty.csv");
+        String header = "id,title,inventory,URL,price,tags";
+        fileWriter.write(header);
+        fileWriter.close();
+        ProductDAO pdDAO = new ProductDAO("empty.csv", new ProductFactory());
 
         UserDataAccessObject userDataAccessObject;
         try {
@@ -60,7 +66,10 @@ public class Main {
         LoggedInView loggedInView = new LoggedInView(loggedInViewModel);
         views.add(loggedInView, loggedInView.viewName);
 
-        viewManagerModel.setActiveView(signupView.viewName);
+        SearchView searchView = SearchUseCaseFactory.create(viewManagerModel, searchViewModel, pdDAO);
+        views.add(searchView, searchView.viewName);
+
+        viewManagerModel.setActiveView(searchView.viewName);
         viewManagerModel.firePropertyChanged();
 
         application.pack();
