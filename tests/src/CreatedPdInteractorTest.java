85447e5dfa7de669016b5d1fe9e20916fb0ea336
Jacccckky <jackyy.shen@mail.utoronto.ca>
2023-12-05 20:49:49 -0500

    add more test cases

---
+++ tests/src/CreatedPdInteractorTest.java
@@ -0,0 +1,61 @@
+package src;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import use_case.create_product.CreatePdInputBoundary;
+import use_case.create_product.CreatePdInteractor;
+import use_case.create_product.CreatePdOutPutBoundary;
+import use_case.create_product.CreatePdInputData;
+import use_case.create_product.CreatePdDAI;
+import entity.Product;
+import entity.ProductFactory;
+
+class CreatedPdInteractorTest {
+
+    @Test
+    void testValidInput() {
+        // Mock dependencies
+        CreatePdOutPutBoundary mockPresenter = Mockito.mock(CreatePdOutPutBoundary.class);
+        CreatePdDAI mockDAO = Mockito.mock(CreatePdDAI.class);
+
+        // Set up interactor
+        CreatePdInputBoundary interactor = new CreatePdInteractor(mockDAO, mockPresenter);
+
+        // Set up valid input data
+        CreatePdInputData validInputData = new CreatePdInputData("Valid Title Greater Than 25 characters", "50.00", "100", "https://example.com/image.jpg", "seller123");
+
+        // Execute interactor
+        interactor.execute(validInputData);
+
+        // Verify that the presenter's success method is called
+        Mockito.verify(mockPresenter).prepareSuccessCreateView("Created Successfully");
+        // Verify that the DAO's save method is called
+        Mockito.verify(mockDAO).save(Mockito.any(), Mockito.eq("seller123"));
+    }
+
+    @Test
+    void testInvalidTitleLength() {
+        // Mock dependencies
+        CreatePdOutPutBoundary mockPresenter = Mockito.mock(CreatePdOutPutBoundary.class);
+        CreatePdDAI mockDAO = Mockito.mock(CreatePdDAI.class);
+
+        // Set up interactor
+        CreatePdInputBoundary interactor = new CreatePdInteractor(mockDAO, mockPresenter);
+
+        // Set up invalid input data (title length < 25)
+        CreatePdInputData invalidInputData = new CreatePdInputData("Short", "50.00", "100", "https://example.com/image.jpg", "seller123");
+
+        // Execute interactor
+        interactor.execute(invalidInputData);
+
+        // Verify that the presenter's failure method is called with the appropriate message
+        Mockito.verify(mockPresenter).prepareFailCreateView("Title must be between 25 and 150 characters.");
+        // Verify that the DAO's save method is not called
+        Mockito.verifyNoInteractions(mockDAO);
+    }
+
+    // Add more test cases for other scenarios (invalid price, invalid inventory, invalid image URL, runtime exception)
+    // ...
+
+}
